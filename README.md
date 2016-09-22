542d6504f0cfc97418ee9dedc4691d458412d746
Abhishek <abhishek.kumar@comprotechnologies.com>
2016-09-22 12:29:03 +0530

    cleanup and readme updates

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
 # runner grid
 
-Steps for connecting a Node to Grid:
+Steps for connecting a Node to Grid & Running Tests:
 
 1. Download selenium jar (current support: selenium-server-standalone-2.41.0.jar), chromedriver.exe & keep in a common directory. eg \jar
-2. Open terminal & move to directory \jar
-3. Run command to connect browser node.
+2. Download browser-connect.bat file (download link on server dashboard right side menu)
+3. Run batch file.
+4. Enter your username. - > your chrome browser node is connected now
+5. Launch grid console to check connected browser. (Navigate from right side menu)
+6. Run test with matching configurations as of registered browser node.
+7. Logs will be displayed on dashboard & test will run on client machine browser.
 
 Examples:
 For Chrome:
---
+++ app/js/util.js
@@ -0,0 +1,54 @@
+/**
+ * Created by AbhishekK on 9/22/2016.
+ */
+
+$(document).ready(function() {
+    var $window = $(window);
+
+    function checkWidth() {
+        var windowsize = $window.width();
+        if (windowsize < 1000) {
+            $('#circle-container').hide();
+            $('#container').width('90%');
+        } else {
+            $('#circle-container').show();
+            $('#container').width('75%');
+        }
+    }
+    // Execute on load
+    checkWidth();
+    // Bind event listener
+    $(window).resize(checkWidth);
+});
+
+$("#grid-url").attr('href', location.protocol+'//'+location.hostname + ':4444/grid/console');
+
+$(document).ready(function() {
+    $(document).delegate('.open', 'click', function(event){
+        $(this).addClass('oppenned');
+        event.stopPropagation();
+    })
+    $(document).delegate('body', 'click', function(event) {
+        $('.open').removeClass('oppenned');
+    })
+    $(document).delegate('.cls', 'click', function(event){
+        $('.open').removeClass('oppenned');
+        event.stopPropagation();
+    });
+
+    $("#script-dn").click(function(){
+        var batFileContent = '@echo off' +'\n' +
+            'echo enter username:' + '\n' +
+            'set /p username=""' +  '\n' +
+            'echo connecting browser node to grid..' + '\n' +
+            'java -jar selenium-server-standalone-2.41.0.jar -role webdriver -hub ' +
+            location.protocol+'//'+location.hostname + ':4444/grid//register -browser browserName="chrome",version=ANY,platform=WINDOWS,maxInstances=5,applicationName=%username% -Dwebdriver.chrome.driver=chromedriver.exe -port 6666'
+        download(batFileContent, "browser-connect.bat", "text/plain");
+    });
+});
+
+
+$('.flip').click(function(){
+    $(this).find('.card').toggleClass('flipped');
+    $('#log-div').css('margin-top','150px');
+});
