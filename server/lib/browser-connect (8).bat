082dd98a253e060a543fec787b1c95a06c34ea11
Abhishek <abhishek.kumar@comprotechnologies.com>
2016-10-06 12:35:16 +0530

    css updates

---
+++ server/lib/browser-connect (8).bat
@@ -0,0 +1,5 @@
+@echo off
+echo enter username:
+set /p username=""
+echo connecting browser node to grid..
+java -jar selenium-server-standalone-2.41.0.jar -role webdriver -hub http://loadrunner1:4444/grid//register -browser browserName="chrome",version=ANY,platform=WINDOWS,maxInstances=5,applicationName=%username% -Dwebdriver.chrome.driver=chromedriver.exe -port 6666
