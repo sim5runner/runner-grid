e65e8f88ce799c04a5119494fe16484891ba5216
abhisheksliam <abhisheksliam@gmail.com>
2017-06-06 14:13:24 +0530

    added java fw

--- server/config/env/production.js
+++ server/config/env/production.js
@@ -4,6 +4,12 @@ var config= {
         "dbURL": "ds023674.mlab.com:23674/runnerv2",
         "username": "root",
         "password": "admin"
+    },
+    api:{
+        script: {
+            java: "http://service-scriptor.herokuapp.com/api/scripts/task/",
+            xml: "http://service-scriptor.herokuapp.com/api/scripts/task/"
+        }
     }
 };
 
