4c785f2f45e7ab45e143714aaa9eeddb15a96222
Abhishek Kumar <abhishek.kumar@comprotechnologies.com>
2017-07-11 16:46:30 +0530

    update in results api payload

--- server/config/env/production.js
+++ server/config/env/production.js
@@ -10,6 +10,13 @@ var config= {
             java: "http://service-scriptor.herokuapp.com/api/scripts/task/",
             xml: "http://service-scriptor.herokuapp.com/api/scripts/task/"
         }
+    },
+    builder : {
+        results : {
+            server: "http://ravis:3000",
+            api: "/api/skilltest/tasks/{task-id}/test-status", // ?step={step-number>}
+            token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjU5NWYzMWJjODI4ZWU0MzYxNDI1NDE0ZiIsInVzZXJWZXJzaW9uIjowLCJ0eXBlIjoxLCJpYXQiOjE0OTk0MTA5ODAsImV4cCI6MTgxNDc3MDk4MH0.ewVIP398aCVirDzs0BpwVxp2Z94phmFOjNtQFlnjLGM"
+        }
     }
 };
 
